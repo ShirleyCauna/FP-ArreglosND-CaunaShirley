c03f9881fc3dfedf9fcd19489deb7cf20852a41e
ShirleyCauna <caunashirley11@gmail.com>
2025-07-05 22:10:01 -0500

    Ejercicio 1 – agregar el enunciado del problema como comentario

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -1,3 +1,11 @@
+/*Producto de Dos Matrices: Implemente un programa que multiplique dos matrices de 3x3 y
+muestre el resultado, y muestre las dos matrices y su resultado en horizontal
+Ejemplo de Salida
+Matriz A:         Matriz B:         Resultado:
+1 2 3             9 8 7              30 24 18
+4 5 6             6 5 4              84 69 54
+7 8 9             3 2 1              138 114 90*/
+
 #include <iostream>
 using namespace std;
 
