9409e9533dd05a9c518887be0240538549bed614
ShirleyCauna <caunashirley11@gmail.com>
2025-07-05 21:49:13 -0500

    Ejercicio 1 – producto de matrices, avance 2: cálculo completo

--- Ejercicio1.cpp
+++ Ejercicio1.cpp
@@ -2,21 +2,43 @@
 using namespace std;
 
 int main() {
-    int matriz1[3][3];
-    int matriz2[3][3];
+    int matriz1[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+    int matriz2[3][3] = {
+        {9, 8, 7},
+        {6, 5, 4},
+        {3, 2, 1}
+    };
+    int producto[3][3] = {0};
 
-    // Ingreso de matriz 1
-    cout << "Ingrese elementos de la matriz 1:\n";
     for (int i = 0; i < 3; ++i)
         for (int j = 0; j < 3; ++j)
-            cin >> matriz1[i][j];
+            for (int k = 0; k < 3; ++k)
+                producto[i][j] += matriz1[i][k] * matriz2[k][j];
 
-    // Ingreso de matriz 2
-    cout << "Ingrese elementos de la matriz 2:\n";
-    for (int i = 0; i < 3; ++i)
+    cout << "Matriz 1:\n";
+    for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j)
-            cin >> matriz2[i][j];
+            cout << matriz1[i][j] << " ";
+        cout << endl;
+    }
 
-    // Aún no se multiplica
+    cout << "\nMatriz 2:\n";
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j)
+            cout << matriz2[i][j] << " ";
+        cout << endl;
+    }
+
+    cout << "\nResultado:\n";
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j)
+            cout << producto[i][j] << " ";
+        cout << endl;
+    }
+
     return 0;
 }
