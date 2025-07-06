604326dc99c11ec6612202c07ee8957afea89cce
ShirleyCauna <caunashirley11@gmail.com>
2025-07-06 01:05:43 -0500

    Ejercicio 4 – intercambio de filas, avance 1: mostrar matriz original

---
+++ Ejercicio4.cpp
@@ -0,0 +1,32 @@
+/* 4. Intercambio de Filas: Desarrolle un programa que intercambie dos filas específicas de una matriz
+de 4x4 y muestre la matriz resultante.
+Ejemplo de Entrada
+Matriz:                    Filas a intercambiar:                 Matriz resultante:
+1 2 3 4                       0                                  9 10 11 12
+5 6 7 8                       2                                  5  6  7  8
+9 10 11 12                                                       1  2  3  4
+13 14 15 16                                                      13 14 15 16 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matriz[4][4] = {
+        { 1,  2,  3,  4 },
+        { 5,  6,  7,  8 },
+        { 9, 10, 11, 12 },
+        {13, 14, 15, 16 }
+    };
+
+    cout << "Contenido de la matriz original:\n";
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Aún no se piden filas ni se intercambian
+
+    return 0;
+}
