9fc28ea7a79bae4e5c7d8fccf8e04c8e402db853
ShirleyCauna <caunashirley11@gmail.com>
2025-07-06 21:08:42 -0500

    Ejercicio 8 – recorrido en espiral, versión final con matriz impresa

--- Ejercicio8.cpp
+++ Ejercicio8.cpp
@@ -19,17 +19,25 @@ using namespace std;
 
 int main() {
     int matriz[5][5] = {
-        {1, 2, 3, 4, 5},
-        {6, 7, 8, 9, 10},
-        {11, 12, 13, 14, 15},
-        {16, 17, 18, 19, 20},
-        {21, 22, 23, 24, 25}
+        {1,  2,  3,  4,  5},
+	{6,  7,  8,  9,  10},
+	{11, 12, 13, 14, 15},
+	{16, 17, 18, 19, 20},
+	{21, 22, 23, 24, 25}
     };
+
+    cout << "Matriz de orden 5:\n";
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            cout << matriz[i][j] << " ";
+        }
+        cout << endl;
+    }
 
     int filaInicio = 0, filaFin = 4;
     int columnaInicio = 0, columnaFin = 4;
 
-    cout << "Recorrido en espiral:\n";
+    cout << "\nRecorrido en espiral:\n";
     while (filaInicio <= filaFin && columnaInicio <= columnaFin) {
         // Fila superior
         for (int j = columnaInicio; j <= columnaFin; j++)
@@ -56,5 +64,6 @@ int main() {
         }
     }
 
+    cout << endl;
     return 0;
 }
